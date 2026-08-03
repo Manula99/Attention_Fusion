797884df1051614eecba60d7b2237b7edb6bb2f4
Manula99 <manula@berkeley.edu>
2026-08-02 21:58:16 -0700

    added set up to modal image

--- modal_accelerate.py
+++ modal_accelerate.py
@@ -4,7 +4,7 @@ from models import U_Transformer
 import os
 os.environ['MONAI_DATA_DIRECTORY'] = '/content/monai_data'
 
-image = modal.Image.debian_slim().pip_install("torch", "monai", "accelerate", "tqdm", "datasets", "transformers")
+image = modal.Image.debian_slim().apt_install("git").pip_install("torch", "monai", "accelerate", "tqdm", "datasets", "transformers").run_commands("git clone https://github.com/Manula99/Attention_Fusion.git && cd Attention_Fusion")
 app = modal.App(image=image)
 
 @app.function(gpu="A100-80GB:3")
