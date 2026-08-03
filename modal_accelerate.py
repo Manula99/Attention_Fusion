4d61bdc326a26fa93062c8c7c0ac5d787e31287e
Manula99 <manula@berkeley.edu>
2026-08-02 22:30:19 -0700

    adding working directory to image

--- modal_accelerate.py
+++ modal_accelerate.py
@@ -1,13 +1,19 @@
 import modal
 
-image = modal.Image.debian_slim().apt_install("git").pip_install("torch", "monai", "accelerate", "tqdm", "datasets", "transformers").run_commands("git clone https://github.com/Manula99/Attention_Fusion.git && cd Attention_Fusion")
+image = (modal.Image.debian_slim()
+         .apt_install("git")
+         .pip_install("torch", "monai", "accelerate", "tqdm", "datasets", "transformers")
+         .run_commands("git clone https://github.com/Manula99/Attention_Fusion.git /root/Attention_Fusion")
+         .workdir("/root/Attention_Fusion"))
+
 app = modal.App(image=image)
 
 @app.function(gpu="A100-80GB:3")
 def run():
     from ddp_computing import distributed_trainer
     from models import U_Transformer
-    import os
+    import sys, os
+    sys.path.insert(0, "/root/Attention_Fusion")
     os.environ['MONAI_DATA_DIRECTORY'] = '/content/monai_data'
 
     model = U_Transformer(4, 3)
